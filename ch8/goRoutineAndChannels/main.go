bc66cbf3e2cc463ada29278b4a2c83ff3b68d58b
usman250994 <mani9418@gmail.com>
2025-09-01 21:14:25 +0200

    wip: simple go routines and channels example

--- ch8/goRoutineAndChannels/main.go
+++ ch8/goRoutineAndChannels/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	ch := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go add(ch, &wg)
+	go get(ch)
+	wg.Wait()
+}
+
+func add(ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+	close(ch)
+}
+
+func get(ch chan int) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("received: %d\n", <-ch)
+	}
 }
